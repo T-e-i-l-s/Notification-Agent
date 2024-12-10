3ccb9a7183f15ff48918e7ea4a49e9f628b3e79d
kmous <kfmust@gmail.com>
2024-12-10 23:56:36 +0300

    Init

---
+++ app/src/main/java/com/mustafin/notifications/utils/notifications/NotificationModel.kt
@@ -0,0 +1,8 @@
+package com.mustafin.notifications.utils.notifications
+
+/* Модель данных из уведомления */
+data class NotificationModel(
+    val source: String,
+    val title: String,
+    val text: String
+)
